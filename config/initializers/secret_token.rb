9b81385979a6b77993ab39b4b66f9a874817062f
Jorge Yau <jorgeyau@gmail.com>
2012-11-21 17:29:36 -0500

    initial commit, created user model, added devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mopxcms::Application.config.secret_token = '1efc3083f2ed391fca6b5b4c8ab25140ebb519c4e19edb3fb2846020769228ccadf1fd977ebc0a3ca65fb1d9354317b1217924b820d928716c931e251785033c'
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Mopxcms::Application.routes.draw do
+  get "home/index"
+
+  root :to => "home#index"
+  devise_for :users
+end
